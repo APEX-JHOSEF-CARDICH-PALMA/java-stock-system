599c97f7f1a7b36942248bd39ac6bf9b51066638
Jhosef Cardich Palma <jacp.tech.91@gmail.com>
2020-08-19 15:04:51 +0200

    First Junit Test working correctly

--- src/main/java/com/jhosefcardichpalma/java/MathUtilities.java
+++ src/main/java/com/jhosefcardichpalma/java/MathUtilities.java
@@ -1,3 +1,4 @@
+package  com.jhosefcardichpalma.java;
 public class MathUtilities {
     public int add (int a, int b){
         System.out.println("Este metodo está corriendo ..");
--- src/test/java/com/jhosefcardichpalma/java/MathUtilitiesTest.java
+++ src/test/java/com/jhosefcardichpalma/java/MathUtilitiesTest.java
@@ -1,3 +1,4 @@
+package com.jhosefcardichpalma.java;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
