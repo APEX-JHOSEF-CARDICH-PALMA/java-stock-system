844d3ed755cf39cf12ab7979f74aceb465717262
Jhosef Cardich Palma <jacp.tech.91@gmail.com>
2020-08-19 18:59:43 +0200

    Classes created: Product, Response, Clothing Store system.

---
+++ src/main/java/com/jhosefcardichpalma/java/Product.java
@@ -0,0 +1,37 @@
+package com.jhosefcardichpalma.java;
+
+public class Product {
+    private int id;
+    private String name;
+    private String description;
+
+    public Product(String name, String description) {
+        this.id=0;
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
